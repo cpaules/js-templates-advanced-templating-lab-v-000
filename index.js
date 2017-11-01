f65acf7639cb042c0453e9578fde1011950656a1
Chase Paules <cpaules@gmail.com>
2017-11-01 19:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function HandlebarsSetup() {
+	Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML)
+	Handlebars.registerPartial("recipeFormPartial", document.getElementById("recipe-form-partial").innerHTML)
+	Handlebars.registerHelper("displayIngredient", function(ingredient) {
+		return new Handlebars.SafeString("<li name='ingredientsList'>" + ingredient + "</li>")
+	})
+}
